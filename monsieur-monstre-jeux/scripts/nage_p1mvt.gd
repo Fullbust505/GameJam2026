f977906589f4eb77053e85e0b45fdcbc6e02017a
Pandadulol <131276038+Pandadulol@users.noreply.github.com>
2026-04-20 14:12:47 +0200

    cam + diff inputs

---
+++ monsieur-monstre-jeux/scripts/nage_p1mvt.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var player_index = 0;
+var input_velocity = Vector2.ZERO;
+var speed = 200;
+
+func _ready():
+	pass
+
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_joy_button_pressed(player_index, JOY_BUTTON_A) and Input.is_action_just_pressed("game_main_button"):
+		input_velocity += Vector2.UP * speed
+	
+	input_velocity+=Vector2.DOWN*delta*$RigidBody2D.gravity_scale*300
+	
+	position+=input_velocity*delta
