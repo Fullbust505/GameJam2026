04409314ee1bede53dd76d00be9e1da41b651c63
Fullbust505 <117484278+Fullbust505@users.noreply.github.com>
2026-04-21 15:36:47 +0200

    added uniform controls + started manger minigame

---
+++ monsieur-monstre-jeux/manger/scripts/player_mvt.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var controls: Resource = null
+
+@export var speed = 40
+@export var rotation_speed = 1.5
+
+var rotation_direction = 0
+
+func _ready() -> void:
+	pass
+
+func get_input():
+	rotation_direction = Input.get_axis(controls.move_left, controls.move_right)
+	velocity = transform.x * Input.get_axis(controls.move_down, controls.move_up) * speed
+	
+func _process(delta: float) -> void:
+	get_input()
+	rotation += rotation_direction * rotation_speed * delta
+	move_and_slide()
