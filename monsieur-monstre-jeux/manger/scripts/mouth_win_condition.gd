d1fb7f2b1f0a1347306e1887a4687230d6b9a244
Fullbust505 <117484278+Fullbust505@users.noreply.github.com>
2026-04-22 00:23:59 +0200

    added feet mechanism

--- monsieur-monstre-jeux/manger/scripts/mouth_win_condition.gd
+++ monsieur-monstre-jeux/manger/scripts/mouth_win_condition.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var animation_player = $AnimationPlayer
 var points = 0;
 
 # Called when the node enters the scene tree for the first time.
@@ -9,7 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	animation_player.play("mouth_loop")
 
 
 func _on_body_entered(body: Node2D) -> void:
--- monsieur-monstre-jeux/manger/scripts/spoon_mechanic.gd
+++ monsieur-monstre-jeux/manger/scripts/spoon_mechanic.gd
@@ -1,27 +1,54 @@
 extends CharacterBody2D
 
 @export var controls: Resource = null
+@export var handler: String = "arm"
 
-@export var speed = 100
+@export var speed = 200
 @export var rotation_speed = 2
 
-@onready var sensor = $SpoonDetectionArea
+@onready var sprite_arm = $ArmSprite
+@onready var sprite_foot = $FeetSprite
+
+@onready var area_arm = $ArmSpoonDetectionArea
+@onready var area_foot = $FootSpoonDetectionArea
+
+@onready var col_arm = $ArmSpoonCollision
+@onready var col_foot = $FeetSpoonCollision
 
 var rotation_direction = 0
 
-func _ready() -> void:
-	pass
+### DECIDE WHICH COLLISION TO USE
+func set_mode():
+	if handler == "arm":
+		sprite_arm.visible = true
+		area_arm.monitoring = true
+		col_arm.set_deferred("disabled", false)
+		
+		sprite_foot.visible = false
+		area_foot.monitoring = false
+		col_foot.set_deferred("disabled", true)
+	if handler == "foot":
+		sprite_arm.visible = false
+		area_arm.monitoring = false
+		col_arm.set_deferred("disabled", true)
+		
+		sprite_foot.visible = true
+		area_foot.monitoring = true
+		col_foot.set_deferred("disabled", false)
 
 ### MOVEMENT FUNCTIONS
 func get_input():
 	rotation_direction = Input.get_axis(controls.move_up, controls.move_down)
 	
-	# ici on peut inverser les inputs gauche droite par haut bas pour le cas des sans bras
 	var horizontal = Input.get_axis(controls.move_left, controls.move_right)
 	var vertical = Input.get_axis(controls.r1, controls.l1)
+	# ici on peut inverser les inputs gauche droite par haut bas pour le cas des sans bras
+	if handler == "arm":
+		pass # default case
+	if handler == "foot":
+		rotation_direction = Input.get_axis(controls.move_down, controls.move_up)
 
 	velocity = (transform.x * horizontal + transform.y * vertical *3)* speed
-	# yes, we do not use transform.y as it is relative to the characters angular position
 
 ### PEA DETECTION
 func _on_spoon_detection_area_body_entered(body: Node2D) -> void:
@@ -42,6 +69,9 @@ func _on_spoon_detection_area_body_exited(body: Node2D) -> void:
 		body.set_collision_mask_value(1, true)
 
 ### MAIN LOOP
+func _ready() -> void:
+	set_mode()
+
 func _process(delta: float) -> void:
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
