05a1fa682b5d712b75d71488f96fe188829b2830
Pandadulol <131276038+Pandadulol@users.noreply.github.com>
2026-04-21 13:28:01 +0200

    cutting game functionnal

---
+++ monsieur-monstre-jeux/scripts/cut_p1.gd
@@ -0,0 +1,30 @@
+extends Node2D
+var speed = 200
+@export var player_index=0
+@export var cut_positions = []
+var input_velocity = Vector2.ZERO
+@onready var timer = $"../mg_duration"
+@onready var minigame = $".."
+signal ready_p1
+signal finish_p1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if not timer.is_stopped():
+		input_velocity = Vector2.ZERO
+		if Input.is_joy_button_pressed(player_index, JOY_BUTTON_A) and Input.is_action_just_pressed("game_main_button") and cut_positions.size() < minigame.number_of_cuts-1:
+			cut_positions.append(position.x)
+			print(cut_positions)
+		input_velocity += Input.get_joy_axis(player_index, JOY_AXIS_LEFT_X) * Vector2.RIGHT * 200
+		
+	if timer.is_stopped() and Input.is_joy_button_pressed(player_index, JOY_BUTTON_A):
+		ready_p1.emit()
+	
+	if cut_positions.size() == minigame.number_of_cuts-1:
+		finish_p1.emit()
+
+	position += input_velocity * delta
+	if position.x > 165:
+		position -= input_velocity * delta
+	elif position.x < -165:
+		position -= input_velocity * delta
